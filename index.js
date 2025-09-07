cbe8bd82cf3fcf599648d5c8a3a107f804345f69
PraneethTechy <praneethg807@gmail.com>
2025-09-07 11:11:11 +0530

    updated index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 const app = express();
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
 app.use(cors());
@@ -30,4 +30,8 @@ app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);    
+});
+
+app.use('/', (req,res) => {
+    res.send("<h1> Welcome to Multi-vendor backend");
 });
